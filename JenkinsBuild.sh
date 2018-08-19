2042a93f59007e162fb39e404b790a2596d21a81
zepplin <jrytterj@hotmail.com>
2018-08-19 21:52:42 +0200

    saas

--- JenkinsBuild.sh
+++ JenkinsBuild.sh
@@ -1,3 +1,3 @@
     mkdir -p buildJenkins
     ( cd buildJenkins ; qmake ../SimpleJenkinsSetup.pro ; make )
-    (cd buildJenkins ; ./SimpleJenkinsSetup -o SimpleJenkinsSetup.test.txt -txt  )
+    (cd buildJenkins ; ./SimpleJenkinsSetup -o SimpleJenkinsSetup.xml -xunitxml  )
