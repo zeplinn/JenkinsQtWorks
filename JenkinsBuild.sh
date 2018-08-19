1d9c34b88ba50c0a515375c00fd932951de149ed
zepplin <jrytterj@hotmail.com>
2018-08-19 21:58:41 +0200

    adas

--- JenkinsBuild.sh
+++ JenkinsBuild.sh
@@ -1,3 +1,3 @@
     mkdir -p buildJenkins
     ( cd buildJenkins ; qmake ../SimpleJenkinsSetup.pro ; make )
-    (cd buildJenkins ; ./SimpleJenkinsSetup -o SimpleJenkinsSetup.xml -xunitxml  )
+    (cd buildJenkins ; ./SimpleJenkinsSetup -o SimpleJenkinsSetup.xml -xml  )
