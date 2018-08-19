983a9b956130c05049a73bba9397d7fe7a392251
zeplinn <jrytterj@hotmail.com>
2018-08-19 19:30:40 +0200

    asfas

--- JenkinsBuild.sh
+++ JenkinsBuild.sh
@@ -1,7 +1,5 @@
 
 
     mkdir -p buildJenkins
-    echo "------------------ build directory created -----------------"
-    ( cd buildJenkins ; qmake ../SimpleJenkinsSetup.pro ; make ; ./SimpleJenkinsSetup )
-    echo "------------------ build directory removed -----------------"
-    rm -r buildJenkins
+    ( cd buildJenkins ; qmake ../SimpleJenkinsSetup.pro ; make ; ./SimpleJenkinsSetup -xunitxml )
+    
