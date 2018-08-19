2354b18f3566de58e845709084b2d1813bd357c0
zepplin <jrytterj@hotmail.com>
2018-08-19 21:40:46 +0200

    build file changed

--- JenkinsBuild.sh
+++ JenkinsBuild.sh
@@ -1,3 +1,3 @@
     mkdir -p buildJenkins
     ( cd buildJenkins ; qmake ../SimpleJenkinsSetup.pro ; make )
-    (cd buildJenkins ; ./SimpleJenkinsSetup -xunitxml > SimpleJenkinsSetup.xml  )
+    (cd buildJenkins ; ./SimpleJenkinsSetup -o SimpleJenkinsSetup.xml -xunitxml  )
